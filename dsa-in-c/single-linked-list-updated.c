dfafae4111f087fbe7a2e2e5347fe7f94eaeed08
ChineduEjiogu1 <chineduejiogu@gmail.com>
2024-02-03 14:33:53 -0500

    linked list

---
+++ dsa-in-c/single-linked-list-updated.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *link;
+};
+
+struct node* add_at_end(struct node *ptr, int data)
+{
+	struct node *temp = malloc(sizeof(struct node));
+	temp->data = data;
+	temp->link = NULL;
+
+	ptr->link = temp;
+	return temp;
+}
+
+void add_beg(struct node **head, int d)
+{
+	struct node *ptr = malloc(sizeof(struct node));
+	ptr->data = d;
+	ptr->link = NULL;
+
+	ptr->link = *head;
+	*head = ptr;
+}
+
+int main()
+{
+	struct node *head = malloc(sizeof(struct node));
+	head->data = 45;
+	head->link = NULL;
+
+	struct node *ptr = malloc(sizeof(struct node));
+	ptr->data = 98;
+	ptr->link = NULL;
+
+	head->link = ptr;
+
+	int data = 3;
+
+	add_beg(&head, data);
+
+	ptr = head;
+
+	while(ptr != NULL)
+	{
+		printf("%d\n", ptr->data);
+		ptr = ptr->link;
+	}
+	return 0;
+}
