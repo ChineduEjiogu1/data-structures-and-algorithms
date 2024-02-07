5a1a7bd576bf03f1fc6abe837887768542479318
ChineduEjiogu1 <chineduejiogu@gmail.com>
2024-02-07 12:48:15 -0500

    added linked list implementation from youtube

--- dsa-in-c/single-linked-list-updated.c
+++ dsa-in-c/single-linked-list-updated.c
@@ -27,23 +27,37 @@ void add_beg(struct node **head, int d)
 	*head = ptr;
 }
 
+void add_at_pos(struct node* head, int data, int pos)
+{
+  struct node *ptr = head;
+  struct node *ptr2 = malloc(sizeof(struct node));
+  ptr2->data = data;
+  ptr2->link = NULL;
+
+  pos--;
+  while(pos != 1)
+  {
+    ptr = ptr->link;
+    pos--;
+  }
+  ptr2->link = ptr->link;
+  ptr->link = ptr2;
+}
+
+
 int main()
 {
 	struct node *head = malloc(sizeof(struct node));
 	head->data = 45;
 	head->link = NULL;
 
-	struct node *ptr = malloc(sizeof(struct node));
-	ptr->data = 98;
-	ptr->link = NULL;
+  add_at_end(head, 98);
+  add_at_end(head, 3);
 
-	head->link = ptr;
+  int data = 67, position = 3;
 
-	int data = 3;
-
-	add_beg(&head, data);
-
-	ptr = head;
+	add_at_pos(head, data, position);
+  struct node *ptr = head;
 
 	while(ptr != NULL)
 	{
